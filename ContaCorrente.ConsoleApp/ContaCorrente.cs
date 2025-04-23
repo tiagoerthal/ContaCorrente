480b9b9db2e10d4e2077d08fa8fdf52d54e40ff0
tiago <tiagoeweber@gmail.com>
2025-04-23 20:14:34 -0300

    Criado metodo de saque

---
+++ ContaCorrente.ConsoleApp/ContaCorrente.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContaCorrente.ConsoleApp
+{
+    public class ContaCorrente
+    {
+        //string[] historicooperacao = new string[100];
+        //int contadorHistorico = 0;
+
+        public int numero;
+        public int limite;
+        //public int sacar;
+        //public int depositar;
+        public  double saldo;
+
+        public static ContaCorrente CriarConta(int numero, int limiteInicial)
+        {
+            ContaCorrente conta = new ContaCorrente();
+            conta.numero = numero;
+            conta.limite = limiteInicial;
+            conta.saldo = 0;
+            return conta;
+        }
+
+        public void obtersaque(double valor)
+        {
+            if (saldo <=0 || valor > saldo)
+            {
+                Console.WriteLine("Saldo insuficiente");
+                return;
+            }
+             saldo -= valor;          
+        }
+
+        //public static double sacar(ContaCorrente conta, double valor)
+       // {
+            //return ContaCorrente.saldo -= valor;            
+       // }
+       
+
+             //(int contador = 0; contador<contadorHistorico; contador++)
+                  // Console.WriteLine(historicooperacao[contador]);
+    }
+}
--- ContaCorrente.ConsoleApp/Program.cs
+++ ContaCorrente.ConsoleApp/Program.cs
@@ -4,7 +4,37 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            ContaCorrente conta1 = new ContaCorrente();
+            conta1.saldo = 1000;
+            conta1.numero = 12;
+            conta1.limite = 0;
+          
+          //  conta1.movimentacoes = new Movimentacao[10];
+
+            conta1.obtersaque(200);
+            Console.WriteLine(conta1.saldo);
+            Console.ReadLine();
+            //conta1.depositar(300);
+
+            //conta1.Depositar(500);
+
+            //conta1.sacar(200);
+
+            ContaCorrente conta2 = new ContaCorrente();
+            conta2.saldo = 300;
+            conta2.numero = 13;
+            conta2.limite = 0;
+            //conta2.movimentacoes = new Movimentacao[10];
+
+            //conta1.TransferirPara(conta2, 400);
+
+            //conta1.ExibirExtrato();
+
+            Console.WriteLine();
+
+            //conta2.ExibirExtrato();
+            Console.ReadLine();
+
         }
     }
 }
